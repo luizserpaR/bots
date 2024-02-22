4b6e883d478b310a27a46dac19f4223006e35fbd
luizserpaR <160978695+luizserpaR@users.noreply.github.com>
2024-02-22 20:50:20 -0300

    enviei arquivos iniciais

---
+++ bot.js
@@ -0,0 +1,44 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async (message) => {
+	if (message.body.toLocaleLowerCase() === 'bom dia') {
+		await message.reply('olá estou a sua disposição, no que posso ajudar?');
+		await message.reply('digite 1 teste');
+		await message.reply('digita 2 teste');
+	}
+	if (message.body.toLocaleLowerCase() === 'boa tarde') {
+		await message.reply('olá estou a sua disposição, no que posso ajudar?');
+		await message.reply('digite 1 teste');
+		await message.reply('digita 2 teste');
+	}
+	if (message.body.toLocaleLowerCase() === 'boa noite') {
+		await message.reply('olá estou a sua disposição, no que posso ajudar?');
+		await message.reply('digite 1 teste');
+		await message.reply('digita 2 teste');
+	}
+	if (message.body.toLocaleLowerCase() === '1') {
+		await message.reply('qual vc gostaria?');
+		await message.reply('digite 3 teste');
+		await message.reply('digita 4 teste');
+	}
+	if (message.body.toLocaleLowerCase() === '2') {
+		await message.reply('qual escolhe?');
+		await message.reply('digite 5 teste');
+		await message.reply('digita 6 teste');
+	}
+	
+});
+
+client.initialize();
+ 
